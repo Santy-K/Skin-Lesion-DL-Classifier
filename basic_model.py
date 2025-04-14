bf1e3ec9deb8d1015248803a2e5a0dfaf1a9aba2
SimonJoubert1 <z5308893@ad.unsw.edu.au>
2025-04-14 20:28:54 +1000

    Basic main changes

--- basic_model.py
+++ basic_model.py
@@ -26,11 +26,11 @@ def main():
     ])
 
     transforms_train = v2.Compose([
-        v2.RandomResizedCrop(scale=(0.8, 1.0), size=image_scale,ratio=(0.9, 1.1)),
-        v2.RandomHorizontalFlip(p=0.5),
-        v2.RandomVerticalFlip(p=0.5),
-        v2.RandomRotation(degrees=5),
-        v2.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4),
+        #v2.RandomResizedCrop(scale=(0.8, 1.0), size=image_scale,ratio=(0.9, 1.1)),
+        #v2.RandomAffine(degrees=90, shear=20),
+        v2.RandomHorizontalFlip(),
+        v2.RandomVerticalFlip(),
+        v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),
         v2.ToImage(),
         v2.ToDtype(torch.float32, scale=True),
         #v2.GaussianNoise(mean=0.0, sigma=0.2),
@@ -86,7 +86,7 @@ def main():
     
     test_loader = DataLoader(
         dataset=test_data,
-        batch_size=batch_size,
+        batch_size=len(test_data),
         shuffle=False,
         num_workers=1,
         pin_memory=True
@@ -94,7 +94,7 @@ def main():
     
     valid_loader = DataLoader(
         dataset=valid_data,
-        batch_size=batch_size,
+        batch_size=len(valid_data),
         shuffle=False,
         num_workers=1,
         pin_memory=True
@@ -112,7 +112,7 @@ def main():
     #2. Hyperparameters, standard for reference
     learning_rate = 0.001
     num_epochs = 75
-    
+    name = "alexnet"
     #3. Model
     model = AlexNetModel()
     #4. Loss and optimizer
@@ -122,20 +122,20 @@ def main():
     #5. Training and testing
     trainer = networkTraining(model, optimizer, criterion)
     for epoch in range(num_epochs):
-        print("Training epoch: ", epoch)      
+        print("Training epoch: ", epoch)   
         trainer.train(train_loader, epoch)
         
         print("Testing epoch: ", epoch)
         trainer.test(valid_loader, "Validation", epoch)
         
         #Save the model
-        trainer.save_model(f"models/alexnet_{epoch}.pth", model_name="history/alexnet")
+        trainer.save_model(f"models/{name}_{epoch}.pth", model_name=f"history/{name}")
 
     #6. Final testing/validation
     trainer.test(test_loader, "Test", num_epochs)
     
     #Save the model after final testing
-    trainer.save_model(f"models/alexnet_{num_epochs}.pth", model_name="history/alexnet")
+    trainer.save_model(f"models/{name}_{num_epochs}.pth", model_name=f"history/{name}")
     
     trainer.plot_model(model_name="Alexnet")
     
